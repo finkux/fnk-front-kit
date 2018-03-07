7e028a36b27db647b72f891c9bdcaccf0869761b
finkux <finkux@gmail.com>
2018-03-07 12:13:20 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kit-1</title>
+    <title>fnk-front-kit</title>
     <link rel="stylesheet" href="dist/css/styles.css">
 </head>
 <body>
-    <h1>svd</h1>
+ 
     <script src="dist/js/scripts.js"></script>
 </body>
 </html>
